90550e06dc030e5179a34ba8dacfef4ab2874c25
mmackey <megan.mackey24@gmail.com>
2023-10-21 03:45:56 -0500

    Display pixels in an even grid
    Uses a calculation to determine flex-basis for each pixel and
    sets that number as an inline style on each pixel. There may be a
    way to do this that is "nicer" than setting an inline style.
    Using CSS Grid for this would be better, but at this point Odin
    Project hasn't had the learner use Grid, so solving problems such
    as this (with the "current" tools available) has become a learning
    process/puzzle on it's own.

--- scripts.js
+++ scripts.js
@@ -7,9 +7,12 @@ const userNum = 2;
 let totalPixels = userNum * userNum;
 console.log(totalPixels);
 
+let flexBasisCalc = (userNum * 100) / totalPixels;
+console.log(flexBasisCalc);
+
 for (let i = 0; i < totalPixels; i++) {
-    console.log(i);
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.style.flexBasis = `${flexBasisCalc}%`;
     canvas.appendChild(pixel);
 }
