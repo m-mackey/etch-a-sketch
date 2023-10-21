97cad3465d913cb58241a078b6bb98c36340d3cb
mmackey <megan.mackey24@gmail.com>
2023-10-21 01:43:44 -0500

    Insert empty divs based on given number

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
+const canvas = document.querySelector('.canvas');
 
-const userNum = 5;
+
+
+const userNum = 2;
 
 let end = userNum * userNum;
 console.log(end);
 
 for (let i = 0; i < end; i++) {
     console.log(i);
-    
+    const pixel = document.createElement('div');
+    canvas.appendChild(pixel);
 }
