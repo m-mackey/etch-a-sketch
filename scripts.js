a9f09efbc458ea0a03be5633767b243fc54b40c5
mmackey <megan.mackey24@gmail.com>
2023-10-24 02:52:50 -0500

    Add function to randomize pixel color on mouseover
    This is kind of neat but I kinda don't like that it takes away the
    ability to draw much. Perhaps the random color could be optional
    instead.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 //placement of pixels
 const canvas = document.querySelector('.canvas');
 
-const userNum = 2;
+const userNum = 4;
 
 let totalPixels = userNum * userNum;
-console.log(totalPixels);
 
 let flexBasisCalc = (userNum * 100) / totalPixels;
-console.log(flexBasisCalc);
 
 for (let i = 0; i < totalPixels; i++) {
   const pixel = document.createElement('div');
@@ -15,6 +13,7 @@ for (let i = 0; i < totalPixels; i++) {
   pixel.style.flexBasis = `${flexBasisCalc}%`;
   pixel.style.opacity = 0;
   pixel.addEventListener('mouseover', setOpacity);
+  pixel.addEventListener('mouseover', randomizeColor);
   canvas.appendChild(pixel);
 }
 
@@ -26,4 +25,12 @@ function setOpacity() {
     currentOpacity += 0.1;
     this.style.opacity = currentOpacity;
   }
+}
+
+//randomize pixel color event
+function randomizeColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  this.style.background = `rgb(${red} ${blue} ${green})`;
 }
